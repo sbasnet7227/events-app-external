631a5a163e767f5d25fd908bc84842ce2e3a26ad
sbasnet7227 <basnet_sunil@hotmail.com>
2022-04-21 16:55:57 +0000

    my first change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'; 
 
-console.log(`process.env.SERVER = ${process.env.SERVER}`);
+console.log(`process.env.SERVER = ${process.env.SERVER}`); // get environment variables
 // get the environment variable, but default to localhost:8082 if its not set
 const SERVER = process.env.SERVER ? process.env.SERVER : "http://localhost:8082";
 
